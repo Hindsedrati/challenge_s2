5701c324a1d8ed36395de8ea03a8cb97482d52e4
Omer Dotchamou <omer.dotchamou@scalens.com>
2023-11-29 14:54:30 +0100

    Update CreateElement by adding createElement and createFunctionalElement method to MiniReact

---
+++ components/Component.js
@@ -0,0 +1,18 @@
+export class Component {
+  constructor(props = {}) {
+    this.props = props;
+  }
+
+  setState(state) {}
+
+  createElement(type, props = {}, events = {}, children = []) {
+    return {
+      type,
+      events,
+      children,
+      props: { ...props },
+    };
+  }
+}
+
+export default Component;
