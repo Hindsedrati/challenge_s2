8d2c86108612b4e04a6ea07c0a7b51987f32cd8a
thomasR92 <thomas.riehl92@gmail.com>
2023-12-14 21:47:50 +0100

    Add loader component but colors doesn't display

--- pages/page1.js
+++ pages/page1.js
@@ -2,14 +2,16 @@ import { BrowserLink } from "../components/BrowserRouter.js";
 import MiniReact from "../core/MiniReact.js";
 // import Card from "../components/Card.js";
 import Link from "../components/Link/Link.js"
+import Button from "../components/button/Button.js";
+import Loader from "../components/Loader/Loader.js";
 
 const data = JSON.parse(localStorage.getItem("data") || "{}");
 
-const accueil = MiniReact.createElement(Link, {
-  title: "Accueil",
-  classes: "text-sm font-semibold leading-6 text-gray-900",
-  href: "/page2",
-});
+// const accueil = MiniReact.createElement(Link, {
+//   title: "Accueil",
+//   classes: "text-sm font-semibold leading-6 text-gray-900",
+//   href: "/page2",
+// });
 
 // const cardData = [
 //   {
@@ -52,14 +54,16 @@ const accueil = MiniReact.createElement(Link, {
 
 // const allCard = cardData.map((data) => MiniReact.createElement(Card, data));
 // console.log(allCard);
-import Button from "../components/button/Button.js";
+
 
 const button = MiniReact.createElement(Button, {
   title: "Click me",
   class:
-    "border border-black rounded-full px-4 py-2 hover:bg-black hover:text-white my-4",
+    "text-grey border border-black rounded-full px-4 py-2 hover:text-custom-blue hover:text-white my-4",
   onClick: () => console.log("Click me"),
 });
+
+const loader = MiniReact.createElement(Loader, {});
 
 export default {
   type: "div",
@@ -69,7 +73,8 @@ export default {
       to: "/page2",
     }),
     button,
-    accueil,
+    loader,
+    // accueil,
     // {
     //   type: "div",
     //   props: {
