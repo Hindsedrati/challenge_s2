b2268f6f1ea6d2e837063e5589af89616d7dabaa
Hind Sedrati <hindsedrati4@gmail.com>
2023-12-25 19:03:31 +0100

    event-detail

--- routes.js
+++ routes.js
@@ -1,12 +1,12 @@
 import page1 from "./pages/page1.js";
 import page2 from "./pages/page2.js";
 import evenements from "./pages/evenements.js";
-// import testPage from "./pages/test-page.js";
+import evenementDetail from "./pages/evenement-detail.js";
 
 export default {
   "/": page1,
   "/page1": page1,
   "/page2": page2,
   "/evenements": evenements,
-  // "./pages/test-page.js": testPage,
+  "/evenement-detail": evenementDetail,
 };
