4fbdf02c4c4aa3b1609d8eb529c76ae0a1079223
Hind Sedrati <hindsedrati4@gmail.com>
2024-01-01 13:17:04 +0100

    header update

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,25 +1,25 @@
 import MiniReact from "../../core/MiniReact.js";
 import Image from "../Image/Image.js";
+import Link from "../Link/Link.js";
 
 const Footer = () => {
 
     return MiniReact.createElement("footer", {
-            class: `h-[100px] md:h-[80px] bg-[#87A397] flex items-center justify-center mt-4 mb-[80px] md:mb-0`,
+            class: `h-[100px] bg-[#87A397] mt-4 mb-[100px] md:mb-0`,
         },
         MiniReact.createElement('div', {
                 class: "flex justify-between items-center gap-4 container mx-auto px-4 py-2",
             },
 
-            MiniReact.createElement("a", {
-                    href: "/",
-                    class: "flex items-center",
-                },
-                MiniReact.createElement(Image, {
-                    src: "../../assets/logo.svg",
+            MiniReact.createElement(Link, {
+                value: MiniReact.createElement(Image, {
+                    src: "assets/logo.svg",
                     alt: "logo",
-                    class: "h-12 w-12"
+                    class: "",
                 }),
-            ),
+                class: "text-sm font-semibold leading-6 text-gray-900",
+                href: "/",
+            }),
 
             MiniReact.createElement("span", {
                     class: "text-white text-xs font-medium",
