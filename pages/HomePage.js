3a728d4925e54e473885800a620b431134df7f40
Hind Sedrati <hindsedrati4@gmail.com>
2024-01-03 00:03:27 +0100

    search-bar

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -2,24 +2,34 @@ import MiniReact from "../core/MiniReact.js";
 import Header from "../components/Header/Header.js";
 import Footer from "../components/Footer/Footer.js";
 import navlinks from "../utils/navlinks.js";
-import Loader from "../components/Loader/Loader.js";
+import SearchInput from "../components/SearchInput/SearchInput.js";
 
 const HomePage = () => {
-  return MiniReact.createElement(
-    "div",
-    null,
-    MiniReact.createElement(Header, {
-      links: navlinks,
-    }),
-    MiniReact.createElement(
-      "div",
-      {
-        class: "h-screen",
-      },
-      "Hola !"
-    ),
-    MiniReact.createElement(Footer)
-  );
+    return MiniReact.createElement(
+        "div",
+        null,
+        MiniReact.createElement(Header,
+            {
+                links: navlinks,
+            }),
+        MiniReact.createElement("div",
+            {
+                class: "container mx-auto"
+            },
+            MiniReact.createElement(SearchInput,
+                {
+                    class: "flex items-center rounded-full shadow-lg bg-white shadow-lg",
+                }),
+            MiniReact.createElement(
+                "div",
+                {
+                    class: "h-screen",
+                },
+                "Hola !"
+            ),
+        ),
+        MiniReact.createElement(Footer)
+    );
 };
 
 export default HomePage;
