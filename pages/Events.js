3f7d89c8516605fbe368c647e073b8e3b3d6c3bf
johnnyhelloworld <johnny58723826@hotmail.fr>
2024-01-03 12:19:01 +0100

    Type de sport filters Discipline and inversely

--- pages/Events.js
+++ pages/Events.js
@@ -23,7 +23,6 @@ class Events extends Component {
         "Sport de raquette",
         "Art martial",
         "Sport de précision",
-        "Haltérophilie",
         "Force",
         "Sport collectif",
         "Athlétisme",
@@ -65,6 +64,8 @@ class Events extends Component {
 
   filterEvents = (filter, discipline) => {
     let filteredEvents = this.state.events;
+    let filteredDisciplines;
+    let filteredTypes;
 
     if (filter !== "all") {
       filteredEvents = filteredEvents.filter((event) => event.type === filter);
@@ -74,6 +75,77 @@ class Events extends Component {
       filteredEvents = filteredEvents.filter(
         (event) => event.discipline === discipline
       );
+    }
+
+    switch (filter) {
+      case "Sport de raquette":
+        filteredDisciplines = ["Badminton", "Tennis de table"];
+        break;
+      case "Art martial":
+        filteredDisciplines = ["Taekwondo"];
+        break;
+      case "Sport de précision":
+        filteredDisciplines = ["Tir à l'arc", "Golf"];
+        break;
+      case "Sport collectif":
+        filteredDisciplines = ["Beach-volley"];
+        break;
+      case "Athlétisme":
+        filteredDisciplines = ["Saut à la perche"];
+        break;
+      case "Force":
+        filteredDisciplines = ["Haltérophilie"];
+        break;
+      default:
+        filteredDisciplines = [
+          "Golf",
+          "Beach-volley",
+          "Tir à l'arc",
+          "Badminton",
+          "Saut à la perche",
+          "Haltérophilie",
+          "Taekwondo",
+          "Tennis de table",
+        ];
+        break;
+    }
+
+    switch (discipline) {
+      case "Golf":
+        filteredTypes = ["Sport de précision"];
+        break;
+      case "Tir à l'arc":
+        filteredTypes = ["Sport de précision"];
+        break;
+      case "Beach-volley":
+        filteredTypes = ["Sport collectif"];
+        break;
+      case "Badminton":
+        filteredTypes = ["Sport de raquette"];
+        break;
+      case "Tennis de table":
+        filteredTypes = ["Sport de raquette"];
+        break;
+      case "Haltérophilie":
+        filteredTypes = ["Force"];
+        break;
+      case "Saut à la perche":
+        filteredTypes = ["Athlétisme"];
+        break;
+      case "Taekwondo":
+        filteredTypes = ["Art martial"];
+        break;
+
+      default:
+        filteredTypes = [
+          "Sport de raquette",
+          "Art martial",
+          "Sport de précision",
+          "Force",
+          "Sport collectif",
+          "Athlétisme",
+        ];
+        break;
     }
 
     const totalPages = Math.ceil(
@@ -86,6 +158,8 @@ class Events extends Component {
       filteredEvents: filteredEvents,
       filter: filter,
       discipline: discipline,
+      disciplines: filteredDisciplines,
+      types: filteredTypes,
       currentPage: newCurrentPage,
     });
   };
