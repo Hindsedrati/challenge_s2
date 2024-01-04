27954e6419b5ec91d2d56127932a51d04eeb57ac
johnnyhelloworld <johnny58723826@hotmail.fr>
2024-01-05 00:22:33 +0100

    change route names in english and add Accueil with Carte

--- utils/navlinks.js
+++ utils/navlinks.js
@@ -1,14 +1,20 @@
 export default [
     {
-        title: "Map",
+        title: "ACCUEIL",
         class: "block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent",
         href: "/",
+        icon: "fa-solid fa-house",
+    },
+    {
+        title: "CARTE",
+        class: "block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent",
+        href: "/map",
         icon: "fa-solid fa-map",
     },
     {
-        title: "Evenements",
+        title: "EVENEMENTS",
         class: "block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent",
-        href: "/evenements",
+        href: "/events",
         icon: "fa-solid fa-calendar",
     }
 ]
