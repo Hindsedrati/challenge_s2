d51bd3d9ea3debab1a28d5c6e3f82679c6ae3f99
Omer Dotchamou <omer.dotchamou@scalens.com>
2024-01-05 13:50:29 +0100

    Setting up error page responsive

--- utils/utils.js
+++ utils/utils.js
@@ -5,7 +5,7 @@ const areObjectsEqual = (obj1, obj2) => {
   return stringifiedObj1 === stringifiedObj2;
 };
 
-const toPascalCase = (string) => {
+const firstLetterToUpperCase = (string) => {
   return string
     .split(" ")
     .map(
@@ -33,34 +33,4 @@ const calculateTimeLeft = () => {
   return timeLeft;
 };
 
-// const calculateTimeLeft = () => {
-//   const updateCountdown = () => {
-//     const jo2024 = new Date("2024-07-26");
-//     const now = new Date();
-//     const difference = jo2024 - now;
-
-//     let timeLeft = {};
-
-//     if (difference > 0) {
-//       timeLeft = {
-//         jours: Math.floor(difference / (1000 * 60 * 60 * 24)),
-//         heures: Math.floor((difference / (1000 * 60 * 60)) % 24),
-//         minutes: Math.floor((difference / 1000 / 60) % 60),
-//         secondes: Math.floor((difference / 1000) % 60),
-//       };
-//     }
-
-//     console.log(timeLeft);
-
-//     // Mettez ici le code pour mettre à jour votre interface utilisateur avec les nouvelles valeurs de timeLeft
-//     // Par exemple, vous pouvez afficher le compte à rebours dans le DOM ou dans la console.
-//   };
-
-//   // Mettez à jour immédiatement au démarrage
-//   updateCountdown();
-
-//   // Mettez à jour toutes les secondes avec setInterval
-//   setInterval(updateCountdown, 1000);
-// };
-
-export { areObjectsEqual, toPascalCase, calculateTimeLeft };
+export { areObjectsEqual, firstLetterToUpperCase, calculateTimeLeft };
