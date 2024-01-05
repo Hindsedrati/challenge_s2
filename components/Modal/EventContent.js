3e9c2bd0bbe7afd506f8fd4e7852acf44ad4a4ef
Hindsedrati <92885675+Hindsedrati@users.noreply.github.com>
2024-01-05 13:40:59 +0100

    Update EventContent.js

--- components/Modal/EventContent.js
+++ components/Modal/EventContent.js
@@ -91,7 +91,7 @@ const EventContent = ({ event }) => {
         },
 
         MiniReact.createElement(Link, {
-          href: `${`http://localhost:3000/event-details?event=` + event.id}`,
+          href: `${`https://challenge.bornenexus.fr/event-details?event=` + event.id}`,
           value: "Plus d'info",
           class:
             "ms-3 text-white bg-[#87A397] hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10",
