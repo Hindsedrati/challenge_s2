5b7cd5802bd665e274bf45e779b1cf90408dafdb
Omer Dotchamou <omer.dotchamou@scalens.com>
2024-01-05 05:21:34 +0100

    Setteing up events list page responsive

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -99,7 +99,7 @@ class HomePage extends Component {
               MiniReact.createElement(Link, {
                 class:
                   "uppercase text-[18px] text-white rounded-[15px] bg-[#87A397] py-[18px] w-full md:w-[212px] text-center hover:bg-[#E9CE6B] hover:text-black transition-all delay-0 duration-150 cursor-pointer",
-                href: "http://localhost:3000/evenements",
+                href: "http://localhost:3000/events",
                 value: "Evenement",
               })
             )
--- utils/navlinks.js
+++ utils/navlinks.js
@@ -1,20 +1,23 @@
 export default [
-    {
-        title: "ACCUEIL",
-        class: "block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent",
-        href: "/",
-        icon: "fa-solid fa-house",
-    },
-    {
-        title: "CARTE",
-        class: "block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent",
-        href: "/map",
-        icon: "fa-solid fa-map",
-    },
-    {
-        title: "EVENEMENTS",
-        class: "block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent",
-        href: "/events",
-        icon: "fa-solid fa-calendar",
-    }
-]
+  {
+    title: "ACCUEIL",
+    class:
+      "block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent",
+    href: "/",
+    icon: "fa-solid fa-house",
+  },
+  {
+    title: "CARTE",
+    class:
+      "block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent",
+    href: "/map",
+    icon: "fa-solid fa-map",
+  },
+  {
+    title: "EVENEMENTS",
+    class:
+      "block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent",
+    href: "/events",
+    icon: "fa-solid fa-calendar",
+  },
+];
